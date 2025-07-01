a680a59f1e9ac08286b19b323c5cddf49ece7d1b
Luka Teng <luka.teng@ingka.ikea.com>
2025-07-01 16:50:00 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: "lukas_bag",
-    script: "npx ts-node src/app.ts",
+    script: "npx ts-node --transpile-only src/app.ts",
     env: {
       "NODE_ENV": "production"
     }
